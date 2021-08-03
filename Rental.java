68bbe8baba2ec210d4bd40b9bcbd8f87a43aeb4a
paulohmf <48098066+paulohmf@users.noreply.github.com>
2021-08-03 02:08:22 +0000

    Commit 9

--- Rental.java
+++ Rental.java
@@ -16,26 +16,8 @@ public class Rental {
       return _movie;
    }
    public double getCharge() {
-      //Adicionar o trecho de código extraído.
-      double thisAmount = 0;
-      //determine amounts for each line
-        switch (_movie.getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (_daysRented > 2)
-                 thisAmount += (_daysRented - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += _daysRented * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (_daysRented > 3)
-                 thisAmount += (_daysRented - 3) * 1.5;
-               break;
-        }
-      return thisAmount;
-    }
+      return _movie.getCharge(_daysRented);
+   }
    public int getFrequentRenterPoints(){
       //Adicionar o trecho de código extraído.
       if ((_movie.getPriceCode() == Movie.NEW_RELEASE) &&
