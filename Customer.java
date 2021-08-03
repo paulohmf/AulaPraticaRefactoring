facc785013e9d5d5a5c7a5ef0bf93697cd02af32
paulohmf <48098066+paulohmf@users.noreply.github.com>
2021-08-03 00:38:37 +0000

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,17 +16,11 @@ public class Customer {
    public String getName (){
       return _name;
    }
-  
-  public String statement() {
-     double totalAmount = 0;
-     int frequentRenterPoints = 0;
-     Enumeration rentals = _rentals.elements();
-     String result = "Rental Record for " + getName() + "\n";
-     while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
+    private double amountFor(Rental each) {
+      //Adicionar o trecho de código extraído.
+      double thisAmount = 0;
+      //determine amounts for each line
         switch (each.getMovie().getPriceCode()) {
            case Movie.REGULAR:
               thisAmount += 2;
@@ -42,6 +36,17 @@ public class Customer {
                  thisAmount += (each.getDaysRented() - 3) * 1.5;
                break;
         }
+      return thisAmount;
+   }
+  
+  public String statement() {
+     double totalAmount = 0;
+     int frequentRenterPoints = 0;
+     Enumeration rentals = _rentals.elements();
+     String result = "Rental Record for " + getName() + "\n";
+     while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        double thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
